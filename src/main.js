84954cf32e21e6dee3c14bb4a357993178eb8f12
Nick <nickdevops6@gmail.com>
2024-06-03 16:56:37 +0300

    upd

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from "vue";
 import App from "./App.vue";
+import router from "./router";
+// import vuerouter library globally
+
 import "./assets/scss/style.scss";
 Vue.config.productionTip = false;
 
+// secondly, whole web app
 new Vue({
+  router,
   render: (h) => h(App),
 }).$mount("#app");
+
+
---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+// import components
+// .....
+
+// firstly, what we use
+Vue.use(VueRouter);
+// setting up the routes
+const router = new VueRouter({
+  routes: [
+    {
+      path: "",
+      component: " ",
+    },
+  ],
+});
+
+export default router;
