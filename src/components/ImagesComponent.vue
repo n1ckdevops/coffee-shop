98bf3015a1c24683985b5a037070657ada7d54db
n1ckdevops <nickdevops6@gmail.com>
2024-06-18 15:51:54 +0300

    Передача аргументов в компонент. Объект props.

--- src/components/ImagesComponent.vue
+++ src/components/ImagesComponent.vue
@@ -1,19 +1,29 @@
 <template>
-  <div class="best__wrapper">
-    <div class="best__item">
-      <img src="@/assets/img/coffee-1.jpg" alt="coffee" />
-      <div class="best__item-title">Solimo Coffee Beans 2kg</div>
-      <div class="best__item-price">10.73$</div>
-    </div>
-    <div class="best__item">
-      <img src="@/assets/img/coffee-2.jpg" alt="coffee" />
-      <div class="best__item-title">Presto Coffee Beans 1kg</div>
-      <div class="best__item-price">15.99$</div>
-    </div>
-    <div class="best__item">
-      <img src="@/assets/img/coffee-3.jpg" alt="coffee" />
-      <div class="best__item-title">AROMISTICO Coffee 1kg</div>
-      <div class="best__item-price">6.99$</div>
-    </div>
+  <div :class="classItem">
+    <img :src="require(`@/assets/img/${image}`)" :alt="image" />
+    <div class="best__item-title">{{ name }}</div>
+    <div class="best__item-price">{{ price }}$</div>
   </div>
 </template>
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    classItem: {
+      type: String,
+      required: false,
+    },
+  },
+};
+</script>
