fd440fc420ef10b8e680629bc2c081ea974ee7fc
Nick <nickdevops6@gmail.com>
2024-07-09 11:03:21 +0300

    upd

--- src/store/coffee.js
+++ src/store/coffee.js
@@ -2,63 +2,50 @@ import { v4 as uuidv4 } from "uuid";
 
 const coffee = {
   state: {
-    coffee: [
-      {
-        id: uuidv4(),
-        image: "coffee-1.jpg",
-        name: "Solimo Coffee Beans 2kg",
-        price: 10.73,
-      },
-      {
-        id: uuidv4(),
-        image: "coffee-2.jpg",
-        name: "Presto Coffee Beans 1kg",
-        price: 15.99,
-      },
-      {
-        id: uuidv4(),
-        image: "coffee-3.jpg",
-        name: "AROMISTICO Coffee 1kg",
-        price: 6.99,
-      },
-      {
-        id: uuidv4(),
-        image: "coffee-1.jpg",
-        name: "Solimo Coffee Beans 2kg",
-        price: 10.73,
-      },
-      {
-        id: uuidv4(),
-        image: "coffee-2.jpg",
-        name: "Presto Coffee Beans 1kg",
-        price: 15.99,
-      },
-      {
-        id: uuidv4(),
-        image: "coffee-3.jpg",
-        name: "AROMISTICO Coffee 1kg",
-        price: 6.99,
-      },
-    ],
+    coffee: [],
+    searchValue: "",
+    sortValue: "",
   },
   mutations: {
     setCoffeeData(state, data) {
       state.coffee = data;
+    },
+    setSearchValue(state, value) {
+      state.searchValue = value;
+    },
+    setSortValue(state, value) {
+      console.log(value);
+      state.sortValue = value;
     },
   },
   actions: {
     setCoffeeData({ commit }, data) {
       commit("setCoffeeData", data);
     },
+    setSearchValue({ commit }, data) {
+      commit("setSearchValue", data);
+    },
+    setSortValue({ commit }, data) {
+      commit("setSortValue", data);
+    },
   },
   getters: {
     getCoffee(state) {
-      return state.coffee;
+      return state.coffee
+        .filter((item) =>
+          item.name.toLowerCase().includes(state.searchValue.toLowerCase())
+        )
+        .filter((item) =>
+          item.country.toLowerCase().includes(state.sortValue.toLowerCase())
+        );
     },
     getCoffeeById(state) {
       return (id) => {
         return state.coffee.find((card) => card.id === id); // Убедитесь, что сравнение строк
       };
+    },
+    getSearchValue(state) {
+      return state.searchValue;
     },
   },
 };
--- src/views/OurCoffeeView.vue
+++ src/views/OurCoffeeView.vue
@@ -52,16 +52,24 @@
                 type="text"
                 placeholder="start typing here..."
                 class="shop__search-input"
+                v-model="searchValue"
               />
+              {{ searchValue }}
             </form>
           </div>
           <div class="col-lg-4">
             <div class="shop__filter">
               <div class="shop__filter-label">Or filter</div>
               <div class="shop__filter-group">
-                <button class="shop__filter-btn">Brazil</button>
-                <button class="shop__filter-btn">Kenya</button>
-                <button class="shop__filter-btn">Columbia</button>
+                <button class="shop__filter-btn" @click="onSort('Brazil')">
+                  Brazil
+                </button>
+                <button class="shop__filter-btn" @click="onSort('Kenya')">
+                  Kenya
+                </button>
+                <button class="shop__filter-btn" @click="onSort('Columbia')">
+                  Columbia
+                </button>
               </div>
             </div>
           </div>
@@ -95,6 +103,14 @@ export default {
     coffee() {
       return this.$store.getters["getCoffee"];
     },
+    searchValue: {
+      set(value) {
+        this.$store.dispatch("setSearchValue", value);
+      },
+      get() {
+        return this.$store.getters["getSearchValue"];
+      },
+    },
   },
   data() {
     return {
@@ -106,8 +122,13 @@ export default {
     fetch("http://localhost:3000/coffee")
       .then((res) => res.json())
       .then((data) => {
-        this.$store.dispatch('setCoffeeData', data)
+        this.$store.dispatch("setCoffeeData", data);
       });
+  },
+  methods: {
+    onSort(value) {
+      this.$store.dispatch("setSortValue", value);
+    },
   },
 };
 </script>
